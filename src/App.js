689855e2e6efec1a0994148eb5cc124df9cd4a93
Maksim Khandeshin <maksimhandeshin@gmail.com>
2025-09-02 19:08:28 +0300

    make hw

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import "./App.css";
+import { ColorsInArray } from "./task6/Array";
+function App() {
+  const myName = "My name is Maksim";
+  const greeting = "Ласкаво просимо до нашого сайту!";
+  const imgUrl =
+    "https://i.pinimg.com/736x/55/e0/e1/55e0e185910208ca747d4082e62eba95.jpg";
+  const imgAlt = "Photo";
+  const favoriteWebSite = {
+    name: "Google",
+    url: "https://www.google.com/",
+  };
+  const a = 110;
+  const b = 69;
+  const result = a + b;
+  return (
+    <div className="App">
+      <h1>{myName}</h1>
+      <p>{greeting}</p>
+      <img src={imgUrl} alt={imgAlt}></img>
+      <br></br>
+      <a href={favoriteWebSite.url}>
+        My favorite web-site is: {favoriteWebSite.name}
+      </a>
+      <p>
+        {a} + {b} = {result}
+      </p>
+      <ColorsInArray></ColorsInArray>
+    </div>
+  );
+}
+
+export default App;
